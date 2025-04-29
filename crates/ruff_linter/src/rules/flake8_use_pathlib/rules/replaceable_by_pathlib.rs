e9d007adfaa82b6ee6fac1490438ca884a35f66d
weartist <weartistt@gmail.com>
2025-04-29 23:54:09 +0800

    fix(PTH208): ignore file descriptors in os.listdir

--- crates/ruff_linter/src/rules/flake8_use_pathlib/rules/replaceable_by_pathlib.rs
+++ crates/ruff_linter/src/rules/flake8_use_pathlib/rules/replaceable_by_pathlib.rs
@@ -99,7 +99,7 @@ pub(crate) fn replaceable_by_pathlib(checker: &Checker, call: &ExprCall) {
             // PTH123
             ["" | "builtins", "open"] => {
                 // `closefd` and `opener` are not supported by pathlib, so check if they are
-                // are set to non-default values.
+                // set to non-default values.
                 // https://github.com/astral-sh/ruff/issues/7620
                 // Signature as of Python 3.11 (https://docs.python.org/3/library/functions.html#open):
                 // ```text
@@ -156,7 +156,16 @@ pub(crate) fn replaceable_by_pathlib(checker: &Checker, call: &ExprCall) {
                 Some(OsReadlink.into())
             }
             // PTH208,
-            ["os", "listdir"] => Some(OsListdir.into()),
+            ["os", "listdir"] => {
+                if call
+                    .arguments
+                    .find_positional(0)
+                    .is_some_and(|expr| is_file_descriptor(expr, checker.semantic()))
+                {
+                    return None;
+                }
+                Some(OsListdir.into())
+            }
             _ => None,
         })
     {
